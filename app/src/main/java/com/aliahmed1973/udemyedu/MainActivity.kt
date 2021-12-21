dccfa15c98456912a3fc9411cdeca22bd97c1789
Ali-Ahmed-1973 <eng_aliahmed1973@hotmail.com>
2021-12-21 17:10:05 +0200

    Create Bottom Navigation

--- app/src/main/java/com/aliahmed1973/udemyedu/MainActivity.kt
+++ app/src/main/java/com/aliahmed1973/udemyedu/MainActivity.kt
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.aliahmed1973.udemyedu.databinding.ActivityMainBinding
 import com.aliahmed1973.udemyedu.network.CourseApi
 import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding= ActivityMainBinding.inflate(layoutInflater)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController= navHost.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+        setContentView(binding.root)
     }
 }
